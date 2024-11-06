3dd1c8535bbcce3f8983cc3f3f677db85fa60d04
Prrost <prrostik@gmail.com>
2024-11-06 14:45:32 +0500

    main fix

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,6 @@ import java.util.Scanner;
 import Model.Facade.TicTacToeFacade;
 
 public class Main {
-    public Main() {
     public static void main(String[] args) {
         try {
             UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
